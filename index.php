22043c3f1e1eca07bded8151731af444be8a1617
Pransun <pransun.bala@gmail.com>
2018-05-18 10:04:17 +0200

    ajout_css

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
     <head>
         <title>Mon SITE</title>  
         <meta charset=UTF-8>  
+        <link rel="stylesheet" href="style.css">
+        <<style>
+        body{background: grey;}
+        p { 
+        position: absolute;
+        font-family: 'Calibri';
+        font-size:50px;
+        color: red;
+        top: 40%;
+        left: 40%;
+
+}
+        </style>
     </head>
     <body>
     <p>Hello !!!</p>
