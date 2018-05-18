d25de4b222b52980599f39c529a11907195e4cfc
Pransun <pransun.bala@gmail.com>
2018-05-18 10:28:58 +0200

    ajout paragraphe + CSS

--- index.php
+++ index.php
@@ -3,8 +3,26 @@
     <head>
         <title>Mon SITE</title>  
         <meta charset=UTF-8>  
+        <<style>
+        body{background: grey}
+        p{position: absolute;
+            top: 40%;
+            left: 40%;
+            font-family: Calibri;
+            font-size: 40px;}
+        i {
+            position: absolute;
+            top: 52%;
+            left: 52%;
+            font-family: Calibri;
+            font-size: 40px;
+            color: black;}
+        }   
+        
+        </style>
     </head>
     <body>
     <p>Hello !!!</p>
+    <i>On est vendredi et c'est bientot le week end</i>
     </body>
 </html>
